f96fda84f7adf5d04d1633be2f67131494e3c8ff
dayzxboxbuilder <dayzxboxbuilder@SRV-DAYZBLDR2.msad.bistudio.com>
2024-10-29 13:29:04 +0100

    Added: Sakhal Halloween mission
    Updated: Enoch Haloween mission

---
+++ halloweenOffline.sakhal/init.c
@@ -0,0 +1,114 @@
+void main()
+{
+	//INIT ECONOMY--------------------------------------
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	//DATE RESET AFTER ECONOMY INIT-------------------------
+	int year, month, day, hour, minute;
+	int reset_month = 2, reset_day = 1;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+
+	if ((month == reset_month) && (day < reset_day))
+	{
+		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+	}
+	else
+	{
+		if ((month == reset_month + 1) && (day > reset_day))
+		{
+			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+		}
+		else
+		{
+			if ((month < reset_month) || (month > reset_month + 1))
+			{
+				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+			}
+		}
+	}
+}
+
+class CustomMission: MissionServer
+{
+	void SetRandomHealth(EntityAI itemEnt)
+	{
+		if ( itemEnt )
+		{
+			float rndHlt = Math.RandomFloat( 0.25, 0.65 );
+			itemEnt.SetHealth01( "", "", rndHlt );
+		}
+	}
+	
+	void SetLowHealth(EntityAI itemEnt)
+	{
+		if ( itemEnt )
+		{
+			float rndHlt = Math.RandomFloat( 0.15, 0.35 );
+			itemEnt.SetHealth01( "", "", rndHlt );
+		}
+	}
+	
+	void SetQuantity(EntityAI itemEnt)
+	{
+		if ( itemEnt )
+		{
+			float rndHlt = Math.RandomInt( 1, 5 );
+			itemEnt.SetQuantity(rndHlt);
+		}
+	}
+
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt;
+		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
+		Class.CastTo( m_player, playerEnt );
+
+		GetGame().SelectPlayer( identity, m_player );
+
+		return m_player;
+	}
+
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+		EntityAI itemClothing;
+		EntityAI itemEnt;
+		ItemBase itemBs;
+		float rand;
+
+		itemClothing = player.FindAttachmentBySlotName( "Body" );
+		if ( itemClothing )
+		{
+			SetRandomHealth( itemClothing );
+			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
+			player.SetQuickBarEntityShortcut(itemEnt, 2);
+			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "SteakKnife" );
+			SetLowHealth( itemEnt );
+			
+			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
+			int rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
+			SetRandomHealth( itemEnt );
+			player.SetQuickBarEntityShortcut(itemEnt, 1);
+		}
+		
+		itemClothing = player.FindAttachmentBySlotName( "Legs" );
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
+		
+		itemClothing = player.FindAttachmentBySlotName( "Feet" );		
+		
+		player.GetStatWater().Set( 750 );
+		player.GetStatEnergy().Set( 1000 );
+		
+		player.SetTemporaryResistanceToAgent(eAgents.INFLUENZA, 900);
+	}
+};
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
