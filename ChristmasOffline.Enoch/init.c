4b9e54d681df753d3be26c81e4ade7aa8da965ea
Pavel Fitz <fitzpav@msad.bistudio.com>
2024-12-17 15:07:37 +0100

    Changed: update Christmas mission files

--- ChristmasOffline.Enoch/init.c
+++ ChristmasOffline.Enoch/init.c
@@ -7,7 +7,7 @@ void main()
 
 	//DATE RESET AFTER ECONOMY INIT-------------------------
 	int year, month, day, hour, minute;
-	int reset_month = 8, reset_day = 10;
+	int reset_month = 7, reset_day = 17;
 	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
 	if ((month == reset_month) && (day < reset_day))
